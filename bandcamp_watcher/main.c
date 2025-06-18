eab37af330f0a24100e799335f5df6ae6d2d07d7
Nick Blievers <nick@blievers.net>
2025-06-18 20:18:31 +1000

    Initial Commit

---
+++ bandcamp_watcher/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  bandcamp_watcher
+//
+//  Created by Nick Blievers on 18/6/2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
